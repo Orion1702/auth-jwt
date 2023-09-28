400ef922ac3b83580113edcfaa7676c44d304026
AlexTa <alex.tarasov.work@gmail.com>
2023-09-28 22:05:42 +0300

    add auth midlewhere

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -5,22 +5,20 @@ const userService = require('../service/user-service');
 
 class UserController {
 	async registration(req, res, next) {
-		try {
-			const errors = validationResult(req);
+        try {
+            const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
             }
-
-			const {email, password} = req.body;
-			const userData = await UserService.registration(email, password);
+            const {email, password} = req.body;
+			const userData = await userService.registration(email, password);
 			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
 
 			return res.json(userData);
-
-		} catch(e) {
-			next(e);
-		}
-	}
+        } catch (e) {
+            next(e);
+        }
+    }
 
 
 	async login(req, res, next) {
--- server/router/index.js
+++ server/router/index.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const userController = require('../controllers/user-controller');
 const router = new Router();
 const { body } = require('express-validator');
+const authMiddleware = require('../middlewares/auth-middleware');
 
 router.post('/registration',
     body('email').isEmail(),
@@ -13,7 +14,7 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUser);
+router.get('/users', authMiddleware, userController.getUser);
 
 module.exports = router;
 
